4b28524ec30402d1c31135d3b13e4f21a27ef939
Darya1705 <darya.zudilina@yandex.ru>
2022-03-17 22:37:42 +0300

    Commit2

---
+++ src/test/java/ru/netology/service/CashbackHackServiceTest.java
@@ -0,0 +1,45 @@
+package ru.netology.service;
+
+import org.testng.annotations.Test;
+
+import static org.testng.AssertJUnit.assertEquals;
+
+public class CashbackHackServiceTest {
+    CashbackHackService cashbackHackService = new CashbackHackService();
+
+    @Test
+    public void shouldCashbackHackService() {
+
+        int actual = cashbackHackService.remain(900);
+        int expected = 100;
+
+        assertEquals(actual, expected);
+    }
+
+    @Test
+    public void shouldCashbackHackService2() {
+
+        int actual = cashbackHackService.remain(800);
+        int expected = 200;
+
+        assertEquals(actual, expected);
+    }
+
+    @Test
+    public void shouldCashbackHackService3() {
+
+        int actual = cashbackHackService.remain(999);
+        int expected = 1;
+
+        assertEquals(actual, expected);
+    }
+
+    @Test
+    public void shouldCashbackHackService4() {
+
+        int actual = cashbackHackService.remain(0);
+        int expected = 1000;
+
+        assertEquals(actual, expected);
+    }
+}
